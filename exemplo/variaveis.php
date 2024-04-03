364ec602c778ad682b80b151b15f6ef4e3c5d8c6
livia-castro <145464728+liviacastro0@users.noreply.github.com>
2024-04-03 12:01:09 -0300

    Add files via upload

---
+++ exemplo/variaveis.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = "Lívia";
+    $sobrenome ="Castro";
+    const PAIS ="Brasil";
+    //
+    //
+    //
+    //
+    //
+    //
+
+    echo"Muito prazer, $nome $sobrenome! Você mora no" . PAIS;
+
+    ?>
+</body>
+</html>
